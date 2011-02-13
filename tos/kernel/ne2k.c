a94e54a99dfa521891e14dc0d249148436ca114b
twp <twp@45013d40-34cb-11e0-8124-a7d033c08936>
2011-02-13 05:29:25 +0000

    ne2k: register addresses and first command set
    add internal register addresses and command register command set
    
    git-svn-id: https://plum.sfsu.edu/svn-720/branches/ndd@16 45013d40-34cb-11e0-8124-a7d033c08936

--- tos/kernel/ne2k.c
+++ tos/kernel/ne2k.c
@@ -1,8 +1,73 @@
 /* NE2K driver for the TOS kernel 
- * divya, babak, and thomas, csc 720 spring 2011
+ * divya and thomas, csc 720 spring 2011
  */
 #include "kernel.h"
 
+#define NE2K_BASE_ADDR	0x300
+
+/* register address assignments, a description of these can be found in
+ * docs/DP8390D.pdf */
+#define NE2K_REG_CR	0x00
+
+/* page 0 (PS1,PS0) = (0,0) */
+/* read: */
+#define NE2K_REG_CLDA0	0x01
+#define NE2K_REG_CLDA1	0x02
+#define NE2K_REG_BNRY	0x03
+#define NE2K_REG_TSR	0x04
+#define NE2K_REG_NCR	0x05
+#define NE2K_REG_FIFO	0x06
+#define NE2K_REG_ISR	0x07
+#define NE2K_REG_CRDA0	0x08
+#define NE2K_REG_CRDA1	0x09
+#define NE2K_REG_RSR	0x0C
+#define NE2K_REG_CNTR0	0x0D
+#define NE2K_REG_CNTR1	0x0E
+#define NE2K_REG_CNTR2	0x0F
+
+/* write: */
+#define NE2K_REG_PSTART	0x01
+#define NE2K_REG_PSTOP	0x02
+#define NE2K_REG_TPSR	0x04
+#define NE2K_REG_TBCR0	0x05
+#define NE2K_REG_TBCR1	0x06
+#define NE2K_REG_RSAR0	0x08
+#define NE2K_REG_RSAR1	0x09
+#define NE2K_REG_RBCR0	0x0A
+#define NE2K_REG_RBCR1	0x0B
+#define NE2K_REG_RCR	0x0C
+#define NE2K_REG_TCR	0x0D
+#define NE2K_REG_DCR	0x0E
+#define NE2K_REG_IMR	0x0F
+
+
+/* page 1 (PS1,PS0) = (0,1) */
+#define NE2K_REG_PAR0	0x01
+#define NE2K_REG_PAR1	0x02
+#define NE2K_REG_PAR2	0x03
+#define NE2K_REG_PAR3	0x04
+#define NE2K_REG_PAR4	0x05
+#define NE2K_REG_PAR5	0x06
+#define NE2K_REG_CURR	0x07
+#define NE2K_REG_MAR0	0x08
+#define NE2K_REG_MAR1	0x09
+#define NE2K_REG_MAR2	0x0A
+#define NE2K_REG_MAR3	0x0B
+#define NE2K_REG_MAR4	0x0C
+#define NE2K_REG_MAR5	0x0D
+#define NE2K_REG_MAR6	0x0E
+#define NE2K_REG_MAR7	0x0F
+
+/* register command sets */
+/* command register */
+#define NE2K_CMD_STP	0x01
+#define NE2K_CMD_STA	0x02
+#define NE2K_CMD_TXP	0x04
+#define NE2K_CMD_RD0	0x08
+#define NE2K_CMD_RD1	0x10
+#define NE2K_CMD_RD2	0x20
+#define NE2K_CMD_PS0	0x40
+#define NE2K_CMD_PS1	0x80
 
 /* turn on ne2k and read data into phy */
 int ne2k_init(struct ne2k_phy *phy) {
